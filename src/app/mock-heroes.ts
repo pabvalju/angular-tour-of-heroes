f79b234c131313b1b36de1a38cfb3ec01d19a8f7
Pablo Vallés <pabvalju@gmail.com>
2019-04-16 18:06:47 +0200

    prueba 1

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Koala' },
+  { id: 2, name: 'Grulla' },
+  { id: 3, name: '<3'},
+  { id: 3, name: 'Te' },
+  { id: 4, name: 'Quiero' },
+  { id: 5, name: 'Mucho' },
+  
+];
